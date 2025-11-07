3e2f3fe89ea1ed9ab6819d8d9b5d4fb952da75ba
minyeong2im <minyeong2im@gmail.com>
2025-11-07 13:16:54 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit",function(e) {
 
     if (input === "임민영님 만세") {
         alert("접속 성공🎉");
-        window.location.href = "main.html";
+        window.location.href = "/WaterBill/main.html";
     } else {
         alert("다시 시도해주세요");
     }
