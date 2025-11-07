5d42c24143e00f74ad255b804aa9a26c88f40c2b
minyeong2im <minyeong2im@gmail.com>
2025-11-07 10:45:52 +0900

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,11 @@
+document.getElementById("loginForm").addEventListener("submit",function(e) {
+    e.preventDefault();
+    const input = document.getElementById("username").value;
+
+    if (input === "임민영님 만세") {
+        alert("접속 성공🎉");
+        window.location.href = "main.html";
+    } else {
+        alert("다시 시도해주세요");
+    }
+});
